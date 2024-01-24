8530ec0b362fbe2e8e18d6c3639dec4bcaa51fe9
HyunA Ko <65843682+EldaKo@users.noreply.github.com>
2024-01-25 01:54:09 +0900

    Create lv.1_30.c

---
+++ lv.1_30.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main() {
+	int n;
+	int i = 0;
+	scanf("%d", &n);
+
+	while (i != n) {
+		i++;
+		printf("%d ", i);
+		
+	}
+
+}
